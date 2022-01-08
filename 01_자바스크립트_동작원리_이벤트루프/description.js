c498fb769a143373e0475b87bc23d9ae617cf2fa
cslim0527 <cslim0527@gmail.com>
2022-01-09 00:09:02 +0900

    01_자바스크립트_동작원리_이벤트루프

---
+++ 01_자바스크립트_동작원리_이벤트루프/description.js
@@ -0,0 +1,43 @@
+// 이벤트 루프에 의한 비동기 이벤트 콜백 함수의 실행
+function func1() {
+  console.log('func1');
+  func2();
+}
+
+function func2() {
+  setTimeout(function () {
+    console.log('func2');
+  }, 0);
+
+  func3();
+}
+
+function func3() {
+  console.log('func3');
+}
+
+func1();
+
+// 이벤트 리스너 동작의 경우 이벤트 루프 (참고 원문: https://poiemaweb.com/js-event)
+function func1() {
+  console.log('func1');
+  func2();
+}
+
+function func2() {
+  // <button class="foo">foo</button>
+  const elem = document.querySelector('.foo');
+
+  elem.addEventListener('click', function () {
+    this.style.backgroundColor = 'indigo';
+    console.log('func2');
+  });
+
+  func3();
+}
+
+function func3() {
+  console.log('func3');
+}
+
+func1();
